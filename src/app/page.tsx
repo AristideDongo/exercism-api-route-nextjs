ff6c5ce4c3cdae36d1fa4a7a90b733ca8108e3f5
AristideDongo <aristidedongo4@gmail.com>
2025-09-27 22:41:11 +0000

    test logger

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,22 @@
+import { ConsoleTransport, Logger, LogLevel } from "glyphlog";
 import Image from "next/image";
 
+const logger = new Logger({
+  level: LogLevel.INFO,
+  transports: [
+    new ConsoleTransport({
+      colors: true,
+      timestamp: true,
+    }),
+  ],
+  defaultMeta: {
+    service: 'user-service',
+    version: '1.0.0',
+  },
+});
+
 export default function Home() {
+logger.info('Application started', { version: '1.0.0', port: 3000 });
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
